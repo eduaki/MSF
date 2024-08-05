fa45fec45c18ba1285a3be3f35210e8057d709ea
Eduardo Alves <edu.alves2003@gmail.com>
2024-08-04 23:02:02 -0300

    saudation system updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,20 @@
 import "./styles.css";
 
 export default function Header() {
-	var saldation;
+	var saudation;
 	var username = "Eduardo" || null;
 
 	var now = new Date();
 
-	if (now.getHours() < 12) saldation = "Bom dia";
-	else if (now.getHours() > 18) saldation = "Boa noite";
+	if (now.getHours >= 5 && now.getHours() < 12) saudation = "Bom dia";
+	else if (now.getHours() > 18) saudation = "Boa noite";
 	else if (now.getHours() <= 18 && now.getHours() >= 12)
-		saldation = "Boa Tarde";
+		saudation = "Boa Tarde";
 
 	return (
 		<div className="container">
 			<h1>
-				{saldation}, ‎ ‎ ‎ ‎ ‎ ‎ {username}
+				{saudation}, ‎ ‎ ‎ ‎ ‎ ‎ {username}
 			</h1>
 		</div>
 	);
